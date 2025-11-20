4a89c5d749d28256e094812f35af545497574ac8
ST10443516 <165780788+MushakararaHazel@users.noreply.github.com>
2025-11-20 14:29:53 +0200

    last push
    final project commit

--- CMCS/CMCS/Data/AppDbContext.cs
+++ CMCS/CMCS/Data/AppDbContext.cs
@@ -9,61 +9,86 @@ namespace CMCS.Data
 
         public DbSet<Claim> Claims => Set<Claim>();
         public DbSet<SupportingDocument> SupportingDocuments => Set<SupportingDocument>();
-
         public DbSet<AppUser> Users { get; set; } = null!;
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             base.OnModelCreating(modelBuilder);
+
             modelBuilder.Entity<AppUser>().HasData(
-    new AppUser
-    {
-        Id = 1,
-        Username = "LECT1",
-        PasswordHash = "pass",
-        FirstName = "Lerato",
-        LastName = "Dlamini",
-        Email = "lecturer@example.com",
-        HourlyRate = 200,
-        Role = UserRole.Lecturer
-    },
-    new AppUser
-    {
-        Id = 2,
-        Username = "COORD1",
-        PasswordHash = "pass",
-        FirstName = "Sifiso",
-        LastName = "Nkosi",
-        Email = "coord@example.com",
-        HourlyRate = 0,
-        Role = UserRole.Coordinator
-    },
-    new AppUser
-    {
-        Id = 3,
-        Username = "MANAGER1",
-        PasswordHash = "pass",
-        FirstName = "Thuli",
-        LastName = "Zulu",
-        Email = "manager@example.com",
-        HourlyRate = 0,
-        Role = UserRole.Manager
-    },
-    new AppUser
-    {
-        Id = 4,
-        Username = "HR1",
-        PasswordHash = "pass",
-        FirstName = "John",
-        LastName = "Moyo",
-        Email = "hr@example.com",
-        HourlyRate = 0,
-        Role = UserRole.HR
-    }
 
+                new AppUser
+                {
+                    Id = 1,
+                    Username = "hazelshaka",
+                    PasswordHash = "pass",
+                    FirstName = "hazel",
+                    LastName = "shaka",
+                    Email = "hazelshaka@gmail.com",
+                    HourlyRate = 200,
+                    Role = UserRole.Lecturer
+                },
 
+                new AppUser
+                {
+                    Id = 2,
+                    Username = "Kim009",
+                    PasswordHash = "pass",
+                    FirstName = "kim",
+                    LastName = "Yam",
+                    Email = "jackharlow@gmail.com",
+                    HourlyRate = 70,
+                    Role = UserRole.Lecturer
+                },
+
+                new AppUser
+                {
+                    Id = 3,
+                    Username = "L2001",
+                    PasswordHash = "pass",
+                    FirstName = "Namjoon",
+                    LastName = "Rm",
+                    Email = "namjoonrm@gmail.com",
+                    HourlyRate = 500,
+                    Role = UserRole.Lecturer
+                },
+
+                new AppUser
+                {
+                    Id = 4,
+                    Username = "COORD1",
+                    PasswordHash = "pass",
+                    FirstName = "john",
+                    LastName = "Baptist",
+                    Email = "nickiminaj@gmail.com",
+                    HourlyRate = 0,
+                    Role = UserRole.Coordinator
+                },
+
+                new AppUser
+                {
+                    Id = 5,
+                    Username = "MANAGER1",
+                    PasswordHash = "pass",
+                    FirstName = "Boss",
+                    LastName = "Baby",
+                    Email = "euphoriajung@gmail.com",
+                    HourlyRate = 0,
+                    Role = UserRole.Manager
+                },
+
+                new AppUser
+                {
+                    Id = 6,
+                    Username = "HR1",
+                    PasswordHash = "pass",
+                    FirstName = "Queen",
+                    LastName = "Bey",
+                    Email = "bambamgot7@gmail.com",
+                    HourlyRate = 0,
+                    Role = UserRole.HR
+                }
             );
         }
-
     }
 }
--- CMCS/CMCS/obj/Debug/net9.0/CMCS.AssemblyInfo.cs
+++ CMCS/CMCS/obj/Debug/net9.0/CMCS.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CMCS")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+62b1207f5c595873a3b921d9105b5a471ed5f8e2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b4a884e86b3e471eeee63c58159e051a0bfab16d")]
 [assembly: System.Reflection.AssemblyProductAttribute("CMCS")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CMCS")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
